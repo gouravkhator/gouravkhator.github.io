736738d606039376f778ab09063d2f38ce62dc94
Gourav Khator <gouravkhator9@gmail.com>
2021-04-15 12:37:50 +0530

    Added outside header click events

--- scripts/nav-script.js
+++ scripts/nav-script.js
@@ -1,6 +1,29 @@
 const hamburger = document.querySelector('.hamburger');
 const header = document.querySelector('header');
 
+window.addEventListener('click', (e) => {
+    const headerWidth = window.getComputedStyle(header, null).width;
+
+    const clickedClasses = e.target.classList;
+
+    const clickedInsideHamburger = clickedClasses.contains('hamburger') || clickedClasses.contains('hamburger-line');
+
+    // if the clicked X axis is greater than header width and the clicked element does not have hamburger or its child classes, then hide the header
+    // as we have dealt with hamburger click events below, so we don't mess it with this
+    if (e.screenX > parseInt(headerWidth) && !clickedInsideHamburger) {
+
+        headerDisplay = window.getComputedStyle(header, null).display;
+
+        if (headerDisplay || headerDisplay !== 'none') {
+            header.style.animation = 'headerDisappear 0.7s ease-in-out normal forwards';
+
+            setTimeout(() => {
+                header.style.display = 'none';
+            }, 700);
+        }
+    }
+});
+
 hamburger.addEventListener('click', () => {
     // if the computed style of header has display: none, then let it appear else disappear.
 
