732cee192ba1a18174e9272a619c88bccf2656ec
Gourav Khator <gouravkhator9@gmail.com>
2021-04-17 19:11:45 +0530

    Polished projects section for smaller screens

--- scripts/nav-script.js
+++ scripts/nav-script.js
@@ -1,26 +1,37 @@
 const hamburger = document.querySelector('.hamburger');
 const header = document.querySelector('header');
 
-window.addEventListener('click', (e) => {
-    const headerWidth = window.getComputedStyle(header, null).width;
+const hideHeaderOuterClick = (e) => {
+    const headerDisplay = window.getComputedStyle(header, null).display;
 
-    const clickedClasses = e.target.classList;
+    if (headerDisplay !== 'none') {
+        // if the header display is not none, then we want to hide it
 
-    const clickedInsideHamburger = clickedClasses.contains('hamburger') || clickedClasses.contains('hamburger-line');
+        const headerWidth = window.getComputedStyle(header, null).width;
+        const clickedClasses = e.target.classList;
 
-    // if the clicked X axis is greater than header width and the clicked element does not have hamburger or its child classes, then hide the header
-    // as we have dealt with hamburger click events below, so we don't mess it with this
-    if (e.screenX > parseInt(headerWidth) && !clickedInsideHamburger) {
+        // is the click inside hamburger or outside hamburger
+        const clickedInsideHamburger = clickedClasses.contains('hamburger') || clickedClasses.contains('hamburger-line');
 
-        headerDisplay = window.getComputedStyle(header, null).display;
+        // if the clicked X axis is greater than header width and the clicked element does not have hamburger or its child classes, then hide the header
+        // as we have dealt with hamburger click events below, so we don't mess it with this
+        if (e.screenX > parseInt(headerWidth) && !clickedInsideHamburger) {
 
-        if (headerDisplay || headerDisplay !== 'none') {
             header.style.animation = 'headerDisappear 0.7s ease-in-out normal forwards';
 
             setTimeout(() => {
                 header.style.display = 'none';
             }, 700);
         }
+    }
+}
+
+window.addEventListener('click', (e) => {
+    const bodyWidth = parseInt(window.getComputedStyle(document.body, null).width);
+
+    // if body width is > 1200px then display the header whether clicked outside or inside the header elements, else hide it as the header will not be displayed then by default (only displayed on hamburger click)
+    if (bodyWidth <= 1200) {
+        hideHeaderOuterClick(e);
     }
 });
 
